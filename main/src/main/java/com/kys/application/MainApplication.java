2ef7ffbc6901838629f278a37c5c411747c56ee6
kys <1409915687@qq.com>
2018-08-13 20:38:52 +0800

    update

--- main/src/main/java/com/kys/application/MainApplication.java
+++ main/src/main/java/com/kys/application/MainApplication.java
@@ -6,8 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.SpringServletContainerInitializer;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 @SpringBootApplication
+@SessionAttributes
 public class MainApplication implements CommandLineRunner {
     @Autowired
     MainTestBean mainPropertiesBean;
