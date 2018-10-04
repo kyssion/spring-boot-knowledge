a3eaa83549a2e191bd5a942e0dfd9a87ed531574
kys <1409915687@qq.com>
2018-10-04 17:22:41 +0800

    update

---
+++ webflux_spring_boot/src/main/java/Config.java
@@ -0,0 +1,29 @@
+public class Config {
+    private String _name;
+    private String _age;
+    private String _value;
+
+    public String get_name() {
+        return _name;
+    }
+
+    public void set_name(String _name) {
+        this._name = _name;
+    }
+
+    public String get_age() {
+        return _age;
+    }
+
+    public void set_age(String _age) {
+        this._age = _age;
+    }
+
+    public String get_value() {
+        return _value;
+    }
+
+    public void set_value(String _value) {
+        this._value = _value;
+    }
+}
